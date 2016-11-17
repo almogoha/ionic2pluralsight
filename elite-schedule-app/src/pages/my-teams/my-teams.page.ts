ca2666419018281fd9ea81e13df3e94b075aaaab
almog ohayon <almog.ohayon@allscripts.com>
2016-11-17 22:59:09 +0200

    initial add

---
+++ elite-schedule-app/src/pages/my-teams/my-teams.page.ts
@@ -0,0 +1,19 @@
+import { Component } from '@angular/core';
+import { NavController } from 'ionic-angular';
+import { TournamentsPage } from '../pages';
+@Component({
+  selector: 'page-my-teams',
+  templateUrl: 'my-teams.page.html'
+})
+export class MyTeamsPage {
+
+  constructor(public navCtrl: NavController) {}
+
+  ionViewDidLoad() {
+    console.log('Hello MyTeamsPage Page');
+  }
+
+  goToTourmaments(){
+    this.navCtrl.push(TournamentsPage);
+  }
+}
---
+++ elite-schedule-app/src/pages/tournaments/tournaments.page.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+import { NavController } from 'ionic-angular';
+@Component({
+  selector: 'page-tournaments',
+  templateUrl: 'tournaments.page.html'
+})
+export class TournamentsPage {
+
+  constructor(public navCtrl: NavController) {}
+
+  ionViewDidLoad() {
+    console.log('Hello TournamentsPage Page');
+  }
+
+}
